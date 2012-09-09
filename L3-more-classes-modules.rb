d6795f945dcae93d0fc0726f09339cc03c0ee94a
Kyle Rames <kyle@rames.org>
2012-09-09 14:21:44 -0500

    updating lessons

--- L3-more-classes-modules.rb
+++ L3-more-classes-modules.rb
@@ -9,6 +9,74 @@ class Media
   end
 
   def to_s
-    "[MEDIA] - #{super}"
+    name
   end
 end
+
+class Track < Media
+  def to_s
+    "[TRACK] #{super}"
+  end
+end
+
+class Movie < Media
+  def to_s
+    "[MOVIE] #{super}"
+  end
+end
+
+track = Track.new("Somebody", "Gotye")
+puts track
+
+movie = Movie.new("Officespace", "Mike Judge")
+puts movie
+
+# Mixins - Ruby's answer to multiple inheritance
+
+module AudioPlayer
+  def play
+    puts "Now playing - #{name}"
+  end
+end
+
+# Let's do it live!
+class Track
+  include AudioPlayer
+end
+
+track.play
+
+# how do we protect against a missing name method?
+module MoviePlayer
+  def play
+    if self.respond_to?(:name)
+      puts "****** Now Playing #{name}"
+    else
+      puts "I don't know how to play that!"
+    end
+  end
+end
+
+class NotAMovie
+  include MoviePlayer
+end
+
+obj = NotAMovie.new
+obj.play
+
+
+#What about a movie?
+class Movie
+  include MoviePlayer
+end
+
+movie.play
+
+# Modules aren't just for mixins, you can use them for namespacing
+module CheapTunes
+  class Track
+  end
+end
+
+CheapTunes::Track.new
+
---
+++ L4-conditionals-loops.rb
@@ -0,0 +1,78 @@
+# Conditionals
+
+is_this_thing_on = true
+
+if is_this_thing_on
+  puts "Yep! It's on!"
+end
+
+# the shorter version
+puts "Yep! It's on!" if is_this_thing_on
+
+#what about the opposite?
+puts "turn it on" unless is_this_thing_on
+
+#what else can we do?
+if is_this_thing_on
+  puts "It's on!"
+else
+  putsh "Turn it on"
+end
+
+def grade_paper(grade)
+  case grade
+  when "A"
+    puts 'Well done!'
+  when "B"
+    puts 'Try harder!'
+  when "C"
+  puts 'You need help!!!'
+  else
+    puts "You just making it up!"
+  end
+end
+
+def grade_test(grade)
+  case grade
+  when "A", "B"
+    puts 'You pretty smart!'
+  when "C", "D"
+    puts 'You pretty dumb!!'
+  else
+    puts "You can't even use a computer!"
+  end
+end
+
+# What about loops?
+i = 0
+while i < 5 do
+  puts i
+  i += 1
+end
+
+for animal in ["dog", "cat", "rat", "bat"] do
+  puts "I just saw a #{animal}"
+end
+
+# Blocks
+(1..10).each {|i| puts i}
+
+(1..10).each {|i| puts i if i % 2 == 1}
+
+# each implemented in ruby?
+def each(array, &block)
+  for i in array do
+    yield i      
+  end
+end
+
+# use it to find the odd numbers
+each([5,6,7,8]) {|num| puts num % 2 == 1 ? "odd" : "even"}
+
+# use blocks for setup and cleanup
+def read_line(file, &block)
+  #implement
+end
+
+# lets revisit the Playlist class - add rating field to track and
+# implement 
